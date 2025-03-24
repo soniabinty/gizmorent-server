ce596cd7b67c43e8f472fe04d4a507556344070e
Sonia Binty <sonianazninbinty@gmail.com>
2025-03-25 04:00:25 +0600

    added pagination of all gadgets

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion ,  ObjectId} = require('mongodb');
+
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
