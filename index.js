72329172e71612bd9a22235b3bf8e602e1316d47
Sonia Binty <sonianazninbinty@gmail.com>
2025-03-24 10:39:29 +0600

    getting data of a specific gadget

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { MongoClient, ServerApiVersion ,  ObjectId} = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -93,6 +93,16 @@ async function run() {
       }
     });
 
+
+    // one gadget by id
+
+    app.get('/gadgets/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+        const result = await gadgetCollection.findOne(query);
+      res.send(result);
+      
+        });
 
     // add from here
 
